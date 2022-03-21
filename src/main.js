49c2bfff94a4fd90e403bf65ea93e221b87ebe9c
Ernesto Diaz <ernesto.adp1@gmail.com>
2022-03-20 22:48:58 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { initializeApp } from "firebase/app";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB69vbVRe2g2is3SSRnCi7EDaNcfTbkRRY",
+    authDomain: "examen-ernesto.firebaseapp.com",
+    projectId: "examen-ernesto",
+    storageBucket: "examen-ernesto.appspot.com",
+    messagingSenderId: "246505405201",
+    appId: "1:246505405201:web:a32f24586f8891d070eab0"
+};
+
+
+
+initializeApp(firebaseConfig);
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+    router,
+    store,
+    render: (h) => h(App)
+}).$mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,72 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Callejon from "../views/Callejon.vue";
+import Login from "../views/Login.vue";
+import Registrar from "../views/Registrar.vue";
+
+import { getAuth } from "firebase/auth";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter);
+
+
+
+
+
+
+
+
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About,
+        meta: {}
+    },
+    {
+        path: "/callejon",
+        name: "Callejon",
+        component: Callejon
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/registrar",
+        name: "Registar",
+        component: Registrar
+    },
+
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    const sesionActiva = getAuth().currentUser;
+    console.log(sesionActiva);
+    const rutaRestringida = to.meta.restringida;
+
+    if (sesionActiva && rutaRestringida) {
+        next();
+    } else if (!sesionActiva && rutaRestringida) {
+        next("/callejon");
+    }
+
+    next();
+});
+
+export default router;
